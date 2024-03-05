7244272aa3b554e8cd1e18326e2fb8252a79587c
Zjoe91 <joseph.awad.91@gmail.com>
2024-03-05 10:38:33 +0100

    added useEffect hook to fetch data from api and triggers symbol prop change

--- debuggingdollars-frontend/src/components/StockInfo.js
+++ debuggingdollars-frontend/src/components/StockInfo.js
@@ -1,1 +1,11 @@
 import React, { useState, useEffect } from "react";
+
+function StockInfo({ symbol }) {
+  const [stockData, setStockData] = useState(null);
+
+  useEffect(() => {
+    fetch(`https://mscbt-integration.ew.r.appspot.com/stockinfo/${symbol}`) // Adjust the URL based on your actual backend URL
+      .then((response) => response.json())
+      .then((data) => setStockData(data))
+      .catch((error) => console.error("Error:", error));
+  }, [symbol]);
