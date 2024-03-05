8798527e55a4a427357c07cb9ed0aa78fc43c89f
Zjoe91 <joseph.awad.91@gmail.com>
2024-03-05 10:40:37 +0100

    added return() to render the portfolio data whenever data fetch is done

--- debuggingdollars-frontend/src/components/StockInfo.js
+++ debuggingdollars-frontend/src/components/StockInfo.js
@@ -11,3 +11,21 @@ function StockInfo({ symbol }) {
   }, [symbol]);
 
   if (!stockData) return <div>Loading stock information...</div>;
+
+  return (
+    <div>
+      <h2>Stock Information for {symbol}</h2>
+      {stockData.map((day, index) => (
+        <div key={index}>
+          <p>
+            Date: {day[0]}, Open: {day[1]["1. open"]}, High: {day[1]["2. high"]}
+            , Low: {day[1]["3. low"]}, Close: {day[1]["4. close"]}, Volume:{" "}
+            {day[1]["5. volume"]}
+          </p>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default StockInfo;
