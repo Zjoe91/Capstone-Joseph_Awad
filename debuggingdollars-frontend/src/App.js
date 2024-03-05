4c01dc389c523bfb82a0a71e8c1706c0a85e4936
Zjoe91 <joseph.awad.91@gmail.com>
2024-03-05 10:16:34 +0100

    added App function for compoenents rendering

--- debuggingdollars-frontend/src/App.js
+++ debuggingdollars-frontend/src/App.js
@@ -1,3 +1,14 @@
 import React from "react";
 import PortfolioInfo from "./components/PortfolioInfo";
 import StockInfo from "./components/StockInfo";
+
+function App() {
+  return (
+    <div>
+      <PortfolioInfo username="user1" />
+      <StockInfo symbol="AAPL" />
+    </div>
+  );
+}
+
+export default App;
