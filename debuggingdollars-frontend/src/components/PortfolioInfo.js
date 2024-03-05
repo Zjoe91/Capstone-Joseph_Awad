cd2d0615bfc6237b1ae22ba790f7951e5e7cc869
Zjoe91 <joseph.awad.91@gmail.com>
2024-03-05 10:27:13 +0100

    added useEffect hook to fetch data from api and triggers username prop change

--- debuggingdollars-frontend/src/components/PortfolioInfo.js
+++ debuggingdollars-frontend/src/components/PortfolioInfo.js
@@ -1,1 +1,15 @@
 import React, { useState, useEffect } from "react";
+
+function PortfolioInfo({ username }) {
+  const [portfolio, setPortfolio] = useState(null);
+
+  useEffect(() => {
+    fetch(
+      `https://mscbt-integration.ew.r.appspot.com/portfolioinfo/${username}`
+    ) // Adjust the URL based on your actual backend URL
+      .then((response) => response.json())
+      .then((data) => setPortfolio(data))
+      .catch((error) => console.error("Error:", error));
+  }, [username]);
+
+  if (!portfolio) return <div>Loading portfolio information...</div>;
