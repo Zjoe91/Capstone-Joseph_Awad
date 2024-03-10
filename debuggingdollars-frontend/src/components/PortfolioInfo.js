dadf6c36b1c70aec7ab64fb77a3e642bd2716e2a
Zjoe91 <joseph.awad.91@gmail.com>
2024-03-10 18:26:55 +0100

    add text style and mouse hover when choose a stock

--- debuggingdollars-frontend/src/components/PortfolioInfo.js
+++ debuggingdollars-frontend/src/components/PortfolioInfo.js
@@ -17,18 +17,43 @@ function PortfolioInfo({ username, onStockSelect, selectedSymbol }) {
   if (!portfolio) return <div>Loading portfolio information...</div>;
 
   // otherwise, return the portfolio information
+  const stockItemStyle = {
+    padding: "10px",
+    margin: "5px 0",
+    cursor: "pointer",
+    borderRadius: "5px",
+    transition: "background-color 0.3s ease", // Smooth transition for hover effect
+  };
+
+  const stockItemSelectedStyle = {
+    ...stockItemStyle,
+    backgroundColor: "#f0f0f0", // Different background for selected item
+    fontWeight: "bold", // Make the selected item bold
+  };
+
   return (
     <div>
       <h2>{username}'s Portfolio</h2>
+      <p style={{ fontSize: "1.2rem", fontWeight: "bold" }}>
+        Click on a Symbol below of your choice to display its history data.
+      </p>
       {portfolio.symbols &&
         Object.entries(portfolio.symbols).map(([symbol, details]) => (
           <div
             key={symbol}
-            style={{
-              background:
-                symbol === selectedSymbol ? "lightgrey" : "transparent",
-            }}
+            style={
+              symbol === selectedSymbol
+                ? stockItemSelectedStyle
+                : stockItemStyle
+            }
             onClick={() => onStockSelect(symbol)}
+            onMouseOver={(e) =>
+              (e.currentTarget.style.backgroundColor = "#e9ecef")
+            } // Lighter shade on hover
+            onMouseOut={(e) =>
+              (e.currentTarget.style.backgroundColor =
+                symbol === selectedSymbol ? "#f0f0f0" : "transparent")
+            } // Revert on mouse out
           >
             <p>
               Symbol: {symbol}, Quantity: {details.quantity}, Value:{" "}
