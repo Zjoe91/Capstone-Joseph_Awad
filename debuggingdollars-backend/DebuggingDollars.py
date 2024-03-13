50461e8b3ede8bb29ffc3b95641e2d8691591f0e
Zjoe91 <joseph.awad.91@gmail.com>
2024-03-13 16:25:08 +0100

    Refactor backend for OracleDB integration and update dependencies.

--- debuggingdollars-backend/DebuggingDollars.py
+++ debuggingdollars-backend/DebuggingDollars.py
@@ -1,17 +1,17 @@
 from flask import Flask, jsonify, request, make_response #importing flask, jsonify, request, make_response
 import requests     #importing requests to make http requests and get data from the web
 from flask_cors import CORS #importing flask_cors to enable cross-origin resource sharing
+import oracledb #importing oracledb to connect to the Oracle database
+
+# Oracle database connection details
+un = 'ADMIN'
+cs = '(description= (retry_count=20)(retry_delay=3)(address=(protocol=tcps)(port=1522)(host=adb.eu-madrid-1.oraclecloud.com))(connect_data=(service_name=g023613c63a0206_capstonedatabase_high.adb.oraclecloud.com))(security=(ssl_server_dn_match=yes)))'
+pw = 'CapstoneDatabase12345'
+# Establish a connection to the Oracle database
+connection = oracledb.connect(user=un, password=pw, dsn=cs)
 
 app = Flask(__name__)   # initializing the flask app
 CORS(app)   # enabling cross-origin resource sharing for the app
-
-# Mock database of user portfolios
-# This is a simple in-memory structure to simulate user stock portfolios
-def user_databse():
-
-    return{
-        'user1': {'AAPL': 10, 'GOOGL': 5, 'AMZN': 3},   
-        }
 
 # Index route for the homepage
 # This route returns a welcome message when accessed
@@ -21,22 +21,25 @@ def index():
 
 # Route to get user portfolio
 # This route accepts a username parameter and returns the stock portfolio for that user
-@app.route('/<userID>', methods=['GET'])
+@app.route('/overview', methods=['GET'])
 
-# It accepts a username as a parameter and returns the user's stock portfolio, including the current value of each stock and the total portfolio value.
+# It returns the user's stock portfolio, including the current value of each stock and the total portfolio value.
 # The portfolio value is calculated by fetching real-time stock prices from an external API.
-def get_portfolio(userID):
-    user_data = user_databse()
-    # Check if the username exists in the mock database
-    if userID not in user_data:
-        return jsonify({'error': 'User not found'}), 404
-
-    users_portfolio_data = user_data[userID]
+def get_portfolio():
+    # UserID hardcoded for this milestone
+    userID= 1
+    # Make a database query to get the user's stock portfolio
+    sql_query= "SELECT STOCKSYMBOL, QUANTITY FROM user_stocks WHERE USERID = :userID"
+    # Execute the query and fetch the user's stock portfolio
+    with connection.cursor() as cursor:
+        cursor.execute(sql_query, userID=userID)
+        user_portfolio = cursor.fetchall()
+    
     total_value = 0 # Total value of the portfolio
     portfolio_data = {} #initialize response dictionary for portfolio data
 
     # Iterate over each stock in the user's portfolio
-    for symbol, quantity in users_portfolio_data.items():
+    for symbol, quantity in user_portfolio:
         # Make an API request to get current stock price for each symbol using function GLOBAL_QUOTE to get real-time stock data
         stock_data_response = requests.get("https://www.alphavantage.co/query", params={
             'function': 'GLOBAL_QUOTE',
